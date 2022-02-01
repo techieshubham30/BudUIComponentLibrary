7054b3761f39c269acdc8dbe887fc2c238d34280
Shubham <techieshubham30@gmail.com>
2022-02-01 17:21:35 +0530

    feat: added alert components

---
+++ Components/Alert/alert.js
@@ -0,0 +1,6 @@
+const alertwithCloseBtn = document.querySelector('.alert-close');
+const alertCloseBtn = document.querySelector('.alert-close-button');
+
+alertCloseBtn.addEventListener("click", ()=>{
+    alertwithCloseBtn.style.display="none";
+})
